97e5caac3d6e429230e65f83cc686744124ce250
pherapont <pherapont@yandex.ru>
2021-04-04 10:12:20 +0700

    creating and deleting links to objects

--- src/ru/konry/stud_fin_assistent/RequestRegistration.java
+++ src/ru/konry/stud_fin_assistent/RequestRegistration.java
@@ -3,13 +3,14 @@ package ru.konry.stud_fin_assistent;
 import ru.konry.stud_fin_assistent.domains.*;
 
 public class RequestRegistration {
-    public static void main(String[] args) {
 
-        StudentRequest sr = createStudentRequest(42);
-
-        saveStudentRequest(sr);
-        System.out.println("Заявка зарегистрирована. Номер заявки " +
-                sr.getStudentRequestId() + " Студент " + sr.getHusband().getName() + " " + sr.getHusband().getSurname());
+    static StudentRequest createStudentRequest(long id) {
+        StudentRequest sr = new StudentRequest();
+        sr.setStudentRequestId(id);
+        StudentRequest sr1 = sr;
+        printStR(sr1);
+        System.out.println("Создана заявка id: " + id);
+        return  sr1;
     }
 
     static void saveStudentRequest(StudentRequest sr) {
@@ -17,10 +18,16 @@ public class RequestRegistration {
                 + sr.getHusband().getName()  + " " + sr.getHusband().getSurname());
     }
 
-    static StudentRequest createStudentRequest(long id) {
-        StudentRequest sr = new StudentRequest();
-        sr.setStudentRequestId(id);
-        System.out.println("Создана заявка id: " + id);
-        return  sr;
+    static void printStR(StudentRequest sreq) {
+        System.out.println("Print request..." + sreq);
+    }
+
+    public static void main(String[] args) {
+
+        StudentRequest sr = createStudentRequest(42);
+
+        saveStudentRequest(sr);
+        System.out.println("Заявка зарегистрирована. Номер заявки " +
+                sr.getStudentRequestId() + " Студент " + sr.getHusband().getName() + " " + sr.getHusband().getSurname());
     }
 }
