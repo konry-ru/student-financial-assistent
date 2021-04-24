ab3c3c963c988ecaaac810a9c606490502bc60d8
konry-ru <konry-ru@yandex.ru>
2021-04-24 07:17:19 +0700

    getConnecton with postgres in separate function

--- src/ru/konry/stud_fin_assistent/dao/DirectoryDao.java
+++ src/ru/konry/stud_fin_assistent/dao/DirectoryDao.java
@@ -8,6 +8,12 @@ import java.util.List;
 
 public class DirectoryDao
 {
+    private Connection getConnection() throws SQLException {
+        Connection con = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/fin_students", "postgres", "postgres"
+        );
+        return con;
+    }
 
     public List<Street> findStreets(String pattern) throws ClassNotFoundException, SQLException {
 
@@ -16,11 +22,13 @@ public class DirectoryDao
                 "UPPER(street_name) LIKE UPPER('%" + pattern + "%')";
 
         Class.forName("org.postgresql.Driver");
-        Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/fin_students", "postgres", "postgres"
-        );
+
+        Connection con = getConnection();
+
         Statement stmnt = con.createStatement();
+
         ResultSet rs = stmnt.executeQuery(query);
+
         while (rs.next()) {
             Street street = new Street(rs.getLong(1), rs.getString(2));
             streets.add(street);
